4dfc10231c4fd829447754bbdd4a3e1d4aa9f43c
Kim <rdt2359@gmail.com>
2022-09-14 16:04:18 +0900

    [45-48] setting: 초기 개발환경 설정

---
+++ src/main/java/com/dangdang/DangdangApplication.java
@@ -0,0 +1,13 @@
+package com.dangdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangdangApplication.class, args);
+	}
+
+}
