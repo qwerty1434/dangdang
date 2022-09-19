51ee16c8ac13034d38c3ca849ee7c06d1f0c1e34
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-09-19 15:42:58 +0900

    [#S07P22A306-120] feat: 카테고리 엔티티 생성

--- backend/src/main/java/com/dangdang/tag/domain/FundingTag.java
+++ backend/src/main/java/com/dangdang/tag/domain/FundingTag.java
@@ -1,12 +1,13 @@
 package com.dangdang.tag.domain;
 
+import com.dangdang.funding.domain.Funding;
 import com.dangdang.member.domain.User;
-import com.sun.istack.NotNull;
 import lombok.*;
 import org.hibernate.annotations.*;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -24,12 +25,13 @@ public class FundingTag {
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     private UUID id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tagId")
     private Tag tag;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fundingId")
     private Funding funding;
-
 }
