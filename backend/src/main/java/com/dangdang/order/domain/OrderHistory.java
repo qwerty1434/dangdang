7c015a04de7024df55268901c6b528b9b9fb2b83
김현지 <sionbu4608@naver.com>
2022-09-19 17:21:03 +0900

    [#S07P22A306-144] feat: 주문내역 엔티티 생성

---
+++ backend/src/main/java/com/dangdang/order/domain/OrderHistory.java
@@ -0,0 +1,61 @@
+package com.dangdang.order.domain;
+
+import com.dangdang.funding.domain.Funding;
+import com.dangdang.member.domain.User;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderHistory {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fund_id", nullable = false)
+    private Funding funding;
+
+    @Column(name = "total_price" , nullable = false)
+    private int totalPrice;
+
+    @Column(name = "address" , nullable = false)
+    private String address;
+
+    @Column(name = "phone_number" , nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email" , nullable = false)
+    private String email;
+
+    public OrderHistory(User user, Funding funding, int totalPrice, String address, String phoneNumber, String email ) {
+        this.user = user;
+        this.funding = funding;
+        this.totalPrice = totalPrice;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+
+
+    }
+
+
+
+}
