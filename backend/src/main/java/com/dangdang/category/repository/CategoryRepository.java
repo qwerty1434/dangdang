7d52b8bd9f0568c24e9a8f6fb7951195d0971187
leemyo <leemyo_@naver.com>
2022-09-20 13:53:04 +0900

    [#S07P22A306-123] feat: 카테고리 CRUD 생성

---
+++ backend/src/main/java/com/dangdang/category/repository/CategoryRepository.java
@@ -0,0 +1,18 @@
+package com.dangdang.category.repository;
+
+import com.dangdang.advice.exceptions.NotFoundException;
+import com.dangdang.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    @Query(value="select type from category", nativeQuery = true)
+    List<String> findAllTypes();
+
+    Category findByType(String type) throws NotFoundException;
+
+}
