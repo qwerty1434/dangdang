119227caecb31e25566f8ef4216a4d72b677e297
김윤석 <rdt2359@gmail.com>
2022-09-23 15:07:05 +0900

    [#S07P22A306-110] feat : EthereumService 펀딩 모금금액 사용 및 조회 기능 추가

--- backend/src/main/java/com/dangdang/blockchain/service/EthereumService.java
+++ backend/src/main/java/com/dangdang/blockchain/service/EthereumService.java
@@ -1,12 +1,14 @@
 package com.dangdang.blockchain.service;
 
 import com.dangdang.blockchain.dto.DepositHistoryResponse;
+import com.dangdang.blockchain.dto.UseHistoryResponse;
 import org.springframework.stereotype.Service;
 import org.web3j.abi.EventEncoder;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.*;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.*;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.admin.Admin;
@@ -19,12 +21,12 @@ import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
@@ -34,7 +36,7 @@ public class EthereumService {
 //    private final static String URL = "http://127.0.0.1:7545";
 
     public final static Admin ADMIN = Admin.build(new HttpService(URL));
-    public final static String DANG_DANG_CONTRACT = "0x7BaD88fe93a914d51110d72278941a9977c5FcCc";
+    public final static String DANG_DANG_CONTRACT = "0x4f553775f834F0a39373B5293b1115FC4b458159";
     public final static String ADMIN_PRIVATE_KEY = "ed21a9034322bc810c282209ad36730e9c5139aa36ad1346ca3d3f3faaab850c";
     public final static BigInteger GAS_PRICE = BigInteger.valueOf(20000000000L);
     public final static BigInteger GAS_LIMIT = BigInteger.valueOf(6721975L);
@@ -42,40 +44,7 @@ public class EthereumService {
     //    테스트넷
     //    private final static String PRIVATE_KEY = "ed21a9034322bc810c282209ad36730e9c5139aa36ad1346ca3d3f3faaab850c";
 
-    public String createAccount() { // private_key 생성
-        try {
-            ECKeyPair keyPair = Keys.createEcKeyPair();
-            return keyPair.getPrivateKey().toString();
-        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException | NoSuchProviderException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // 블록체인 내 스마트 컨트랙트 생성 >> 15초 - 1분 >> 승인처리 되면 진행
-    public void createFundingInBlockChain(String fundingId, String makerPrivateKey) {
-        Function function = new Function("createFunding",
-                Arrays.asList(new Utf8String(fundingId), new Address(getAddressFromPrivateKey(makerPrivateKey))),
-                Collections.emptyList());
-        try {
-            String txHash = ethSendRawTransaction(function, ADMIN_PRIVATE_KEY, DANG_DANG_CONTRACT);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // 펀딩 구매 시 입금
-    public void sendMoneyToFunding(String fundingId, String nickname, String buyerPrivateKey, boolean isAnonymous, String ether) {
-        Function function = new Function("sendMoneyToFunding",
-                Arrays.asList(new Utf8String(fundingId), new Utf8String(nickname), new Bool(isAnonymous)),
-                Collections.emptyList());
-        try {
-            ethSendRawTransactionWithEther(function, buyerPrivateKey, DANG_DANG_CONTRACT, ether);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // 펀딩 상태 변경(모금 가능 상태로)
+    // [1] 펀딩 진행중 상태로 변경 (모금 가능 상태)
     public void setFundingStateToFundRaising(String fundingId) {
         Function function = new Function("setFundingStatusFundRaising",
                 Arrays.asList(new Utf8String(fundingId)),
@@ -87,7 +56,7 @@ public class EthereumService {
         }
     }
 
-    // 펀딩 상태 변경(성공적인 종료 시)
+    // [2] 펀딩 상태 변경, 판매자 물품 제작 단계(성공적인 종료 시)
     public void setFundingStatusProduction(String fundingId) {
         Function function = new Function("setFundingStatusProduction",
                 Arrays.asList(new Utf8String(fundingId)),
@@ -99,7 +68,7 @@ public class EthereumService {
         }
     }
 
-    // 펀딩이 완전히 끝났을 때
+    // [3] 펀딩이 성공적으로 완전히 끝났을 때
     public void setFundingStatusDone(String fundingId) {
         Function function = new Function("setFundingStatusDone",
                 Arrays.asList(new Utf8String(fundingId)),
@@ -111,7 +80,95 @@ public class EthereumService {
         }
     }
 
-    // 모금 히스토리 (서포터 히스토리)
+    // [4] 펀딩 실패 상태로 변경 및 환불
+    public void setFundingStatusFail(String fundingId) {
+        Function function = new Function("setFundingStatusFail",
+                Arrays.asList(new Utf8String(fundingId)),
+                Collections.emptyList());
+        try {
+            String txHash = ethSendRawTransaction(function, ADMIN_PRIVATE_KEY, DANG_DANG_CONTRACT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [5] 블록체인 내 펀딩 스마트 컨트랙트 생성
+    public void createFundingInBlockChain(String fundingId, String makerPrivateKey) {
+        Function function = new Function("createFunding",
+                Arrays.asList(new Utf8String(fundingId), new Address(getAddressFromPrivateKey(makerPrivateKey))),
+                Collections.emptyList());
+        try {
+            String txHash = ethSendRawTransaction(function, ADMIN_PRIVATE_KEY, DANG_DANG_CONTRACT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [6] 펀딩 구매 시 입금
+    public void sendMoneyToFunding(String fundingId, String nickname, String buyerPrivateKey, boolean isAnonymous, int won) {
+        Function function = new Function("sendMoneyToFunding",
+                Arrays.asList(new Utf8String(fundingId), new Utf8String(nickname), new Bool(isAnonymous)),
+                Collections.emptyList());
+
+        try {
+            ethSendRawTransactionWithWei(function, buyerPrivateKey, DANG_DANG_CONTRACT, changeWonToWei(won));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [7] 펀딩 금액 사용
+    public void sendMoneyToManufacture(String fundingId, String factoryAddress, int won, String purpose) {
+        BigDecimal amount = Convert.toWei(changeWonToWei(won), Convert.Unit.WEI);
+
+        Function function = new Function("sendMoneyToProductManufacture",
+                Arrays.asList(new Utf8String(fundingId), new Address(factoryAddress),
+                        new Uint256(new BigInteger(amount.toString())), new Utf8String(purpose)),
+                Collections.emptyList());
+        try {
+            String txHash = ethSendRawTransaction(function, ADMIN_PRIVATE_KEY, DANG_DANG_CONTRACT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [8] 펀딩 남은 금액 조회
+    public int checkLeftMoneyInFunding(String fundingId, String privateKey) {
+        Function function = new Function("checkLeftMoneyInFund",
+                Arrays.asList(new Utf8String(fundingId)),
+                Arrays.asList(new TypeReference<Uint256>() {
+                }));
+        try {
+            String wei = (String) ethCall(function, getAddressFromPrivateKey(privateKey), DANG_DANG_CONTRACT);
+            return changeWeiToWon(wei);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    // [9] 펀딩 상태 조회
+//    public String checkFundingStatus(String fundingId, String privateKey) {
+//        Function function = new Function("checkFundingStatus",
+//                Arrays.asList(new Utf8String(fundingId)),
+//                Collections.emptyList());
+//        try {
+//            return (String) ethCall(function, getAddressFromPrivateKey(privateKey), DANG_DANG_CONTRACT);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    // 프라이빗 키 생성
+    public String createAccount() { // private_key 생성
+        try {
+            ECKeyPair keyPair = Keys.createEcKeyPair();
+            return keyPair.getPrivateKey().toString(16);
+        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [101] 모금 히스토리 (서포터 히스토리)
     public List<DepositHistoryResponse> getDepositHistory(String fundingId) {
         ArrayList<DepositHistoryResponse> ret = new ArrayList<>();
 
@@ -133,40 +190,60 @@ public class EthereumService {
             List<Type> nonIndexedValues = FunctionReturnDecoder.decode(log.getData(), event.getNonIndexedParameters());
 
             String address = nonIndexedValues.get(0).getValue().toString();
-            String money = nonIndexedValues.get(1).getValue().toString();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = new Date();
-            date.setTime(Long.parseLong(nonIndexedValues.get(2).getValue().toString()));
+            int won = changeWeiToWon(nonIndexedValues.get(1).getValue().toString());
+            Date date = changeUnixTimeToDate(nonIndexedValues.get(2).getValue().toString());
             String nickname = nonIndexedValues.get(3).getValue().toString();
             String isAnonymous = nonIndexedValues.get(4).getValue().toString();
-            ret.add(new DepositHistoryResponse(address, money, new Timestamp(date.getTime()), nickname, Boolean.parseBoolean(isAnonymous)));
+            ret.add(new DepositHistoryResponse(address, won, new Timestamp(date.getTime()), nickname, Boolean.parseBoolean(isAnonymous)));
         });
         return ret;
     }
 
-    private Object ethCall(String from, String contract, Function function) throws IOException {
-        Transaction transaction = Transaction.createEthCallTransaction(from, contract,
+    // [102] 사용 히스토리 (서포터 히스토리)
+    public List<UseHistoryResponse> getUseHistory(String fundingId) {
+        ArrayList<UseHistoryResponse> ret = new ArrayList<>();
+
+        Web3j web3j = Web3j.build(new HttpService(URL));
+        org.web3j.protocol.core.methods.request.EthFilter filter = new EthFilter(DefaultBlockParameterName.EARLIEST, DefaultBlockParameterName.LATEST, DANG_DANG_CONTRACT);
+        Event event = new Event("UseHistory", Arrays.<TypeReference<?>>asList(
+                new TypeReference<Utf8String>(true) {
+                }, new TypeReference<Address>(false) {
+                }, new TypeReference<Uint>(false) {
+                }, new TypeReference<Uint>(false) {
+                }, new TypeReference<Uint>(false) {
+                }, new TypeReference<Utf8String>(false) {
+                }));
+        String topic = EventEncoder.encode(event);
+        filter.addSingleTopic(topic);
+        filter.addOptionalTopics(Hash.sha3String(fundingId));
+
+        web3j.ethLogFlowable(filter).subscribe(log -> {
+            List<Type> nonIndexedValues = FunctionReturnDecoder.decode(log.getData(), event.getNonIndexedParameters());
+
+            String factoryAddress = nonIndexedValues.get(0).getValue().toString();
+            int spendMoney = changeWeiToWon(nonIndexedValues.get(1).getValue().toString());
+            int leftMoney = changeWeiToWon(nonIndexedValues.get(2).getValue().toString());
+            Date date = changeUnixTimeToDate(nonIndexedValues.get(3).getValue().toString());
+            String purpose = nonIndexedValues.get(4).getValue().toString();
+
+            ret.add(new UseHistoryResponse(factoryAddress, spendMoney, leftMoney, new Timestamp(date.getTime()), purpose));
+        });
+        return ret;
+    }
+
+    private String ethCall(Function function, String address, String contract) throws IOException {
+        Transaction transaction = Transaction.createEthCallTransaction(address, contract,
                 FunctionEncoder.encode(function));
-
         EthCall ethCall = ADMIN.ethCall(transaction, DefaultBlockParameterName.LATEST).send();
-
         List<Type> decode = FunctionReturnDecoder.decode(ethCall.getResult(),
                 function.getOutputParameters());
-
-        System.out.println("ethCall.getResult() = " + ethCall.getResult());
-        System.out.println("getValue = " + decode.get(0).getValue());
-        System.out.println("getType = " + decode.get(0).getTypeAsString());
-
-        return decode.get(0).getValue();
+        return decode.get(0).getValue().toString();
     }
 
     private String ethSendRawTransaction(Function function, String privateKey, String contract) throws IOException {
         EthGetTransactionCount ethGetTransactionCount = ADMIN.ethGetTransactionCount(
                 getAddressFromPrivateKey(privateKey), DefaultBlockParameterName.PENDING).send();
         BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-
-        BigInteger GAS_PRICE = BigInteger.valueOf(20000000000L);
-        BigInteger GAS_LIMIT = BigInteger.valueOf(6721975L);
 
         RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, GAS_PRICE,
                 GAS_LIMIT, contract, FunctionEncoder.encode(function));
@@ -184,38 +261,27 @@ public class EthereumService {
         return ethSendTransaction.getTransactionHash();
     }
 
-    private void ethSendRawTransactionWithEther(Function function, String privateKey, String contract, String ether) throws IOException {
+    private void ethSendRawTransactionWithWei(Function function, String privateKey, String contract, String wei) throws IOException {
         EthGetTransactionCount ethGetTransactionCount = ADMIN.ethGetTransactionCount(
                 getAddressFromPrivateKey(privateKey), DefaultBlockParameterName.LATEST).send();
         BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-        System.out.println(nonce);
-        BigInteger value = Convert.toWei(ether, Convert.Unit.ETHER).toBigInteger();
-        System.out.println(value);
-        //----
-        String encodedFunction = FunctionEncoder.encode(function);
+        BigInteger value = Convert.toWei(wei, Convert.Unit.WEI).toBigInteger();
         RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, GAS_PRICE,
                 GAS_LIMIT, contract, value, FunctionEncoder.encode(function));
         Credentials credentials = Credentials.create(privateKey);
         byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
         String hexValue = Numeric.toHexString(signedMessage);
-        EthSendTransaction ethSendTransaction = ADMIN.ethSendRawTransaction(hexValue).send();
-
-//        Transaction transaction = Transaction.createFunctionCallTransaction(
-//                getAddressFromPrivateKey(privateKey), nonce, GAS_PRICE, GAS_LIMIT, contract, value, encodedFunction);
-//        EthSendTransaction transactionResponse = ADMIN.ethSendTransaction(transaction).send();
+        ADMIN.ethSendRawTransaction(hexValue).send();
     }
 
     private TransactionReceipt getReceipt(String transactionHash) throws IOException {
-
         EthGetTransactionReceipt transactionReceipt = ADMIN.ethGetTransactionReceipt(transactionHash).send();
-
         if (transactionReceipt.getTransactionReceipt().isPresent()) {
             System.out.println("transactionReceipt.getResult().getContractAddress() = " +
                     transactionReceipt.getResult());
         } else {
             System.out.println("transaction complete not yet");
         }
-
         return transactionReceipt.getResult();
     }
 
@@ -227,5 +293,19 @@ public class EthereumService {
     private String getPublicKeyFromPrivateKey(String privateKey) {
         Credentials cs = Credentials.create(privateKey);
         return cs.getEcKeyPair().getPublicKey().toString(16);
+    }
+
+    private String changeWonToWei(int won) {
+        //TODO won이 0,1일떄 처리 추가
+        return String.valueOf(won / 10);
+    }
+
+    private int changeWeiToWon(String wei) {
+        return Integer.parseInt(wei) * 10;
+    }
+
+    public Date changeUnixTimeToDate(String unixTimeStamp) {
+        long unix_seconds = Long.parseLong(unixTimeStamp);
+        return new Date(unix_seconds * 1000L);
     }
 }
--- backend/src/test/java/com/dangdang/DangDangFundingTest.java
+++ backend/src/test/java/com/dangdang/DangDangFundingTest.java
@@ -1,6 +1,7 @@
 package com.dangdang;
 
 import com.dangdang.blockchain.dto.DepositHistoryResponse;
+import com.dangdang.blockchain.dto.UseHistoryResponse;
 import com.dangdang.blockchain.service.EthereumService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @SpringBootTest
 public class DangDangFundingTest {
-//
+
 //    private final static String PRIVATE_KEY = EthereumService.ADMIN_PRIVATE_KEY;
 //
 ////    테스트넷
@@ -20,26 +21,32 @@ public class DangDangFundingTest {
 //    @Autowired
 //    EthereumService ethereumService;
 //
-//    private final String UUID = "550e8400-e29b-41d4-a716-446655440000";
+//    private final String UUID = "550e8400-e29b-41d4-a716-446655440002";
+//    private final String tempAddress1 = "0x0A50Ca4c71bf88fD4f08907bf7aD30244eFfB118";
+//    private final String tempAddress2 = "";
 //
+//    //[5]
 //    @Test
 //    public void createFunding() {
 //        System.out.println("Create Funding");
 //        ethereumService.createFundingInBlockChain(UUID, PRIVATE_KEY);
 //    }
 //
+//    //[1]
 //    @Test
 //    public void setFundingStateToStart() {
 //        System.out.println("Set fundingState to start");
 //        ethereumService.setFundingStateToFundRaising(UUID);
 //    }
 //
+//    // [6]
 //    @Test
 //    public void buyFunding() {
 //        System.out.println("Buy Funding");
-//        ethereumService.sendMoneyToFunding(UUID, "test", PRIVATE_KEY, false, "0.02");
+//        ethereumService.sendMoneyToFunding(UUID, "test3", PRIVATE_KEY, false, 1000);
 //    }
 //
+//    // [101] 모금 히스토리 (서포터 히스토리)
 //    @Test
 //    public void seeDepositHistory() {
 //        System.out.println("get Deposit History");
@@ -51,24 +58,41 @@ public class DangDangFundingTest {
 //        }
 //    }
 //
+//    // [2]
 //    @Test
-//    public void createFundingAndSendMoney() {
-//        System.out.println("Create Funding");
-//        ethereumService.createFundingInBlockChain(UUID, PRIVATE_KEY);
+//    public void setFundingStatusProduction() {
+//        System.out.println("Set fundingState to production");
+//        ethereumService.setFundingStatusProduction(UUID);
+//    }
 //
-//        System.out.println("Set fundingState to start");
-//        ethereumService.setFundingStateToFundRaising(UUID);
+//    // [7] 펀딩 금액 사용
+//    @Test
+//    public void sendMoneyToManufacture() {
+//        System.out.println("send Money To Manufacture");
+//        ethereumService.sendMoneyToManufacture(UUID, tempAddress1, 450, "테스트 목적");
+//    }
 //
-//        System.out.println("Buy Funding");
-//        ethereumService.sendMoneyToFunding(UUID, "닉네임", PRIVATE_KEY, false,"1");
+//    // [8] 펀딩 남은 금액 조회
+//    @Test
+//    public void checkLeftMoneyInFunding() {
+//        System.out.println("check Left Money In Funding");
+//        System.out.println("left money " + ethereumService.checkLeftMoneyInFunding(UUID, tempAddress1));
+//    }
 //
-//        System.out.println("get Deposit History");
+//    // [102] 사용 히스토리 (서포터 히스토리)
+//    @Test
+//     public void getUseHistory() {
+//        System.out.println("get Use History");
 //
-//        List<DepositHistoryResponse> depositHistory = ethereumService.getDepositHistory(UUID);
+//        List<UseHistoryResponse> useHistory = ethereumService.getUseHistory(UUID);
 //
-//        for (DepositHistoryResponse d : depositHistory) {
+//        for (UseHistoryResponse d : useHistory) {
 //            System.out.println(d);
 //        }
 //    }
-
+//
+//    @Test
+//    public void generatePrivateKey(){
+//        System.out.println(ethereumService.createAccount());
+//    }
 }
