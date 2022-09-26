3184c097c1ea65205d03ad277ac761761daeff99
DESKTOP-KVCQHCD\SSAFY <yi_jonghoon@naver.com>
2022-09-26 11:32:13 +0900

    applied fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,4 +15,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+@font-face {
+  font-family: "NanumSquare";
+  font-weight: 400;
+  src: url("@/assets/fonts/NanumSquareR.ttf");
+  src: url("@/assets/fonts/NanumSquareR.ttf") format("truetype");
+}
+@font-face {
+  font-family: "NanumSquare";
+  font-weight: 700;
+  src: url("@/assets/fonts/NanumSquareB.ttf");
+  src: url("@/assets/fonts/NanumSquareB.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "NanumSquare";
+  font-weight: 300;
+  src: url("@/assets/fonts/NanumSquareL.ttf");
+  src: url("@/assets/fonts/NanumSquareL.ttf") format("truetype");
+}
+</style>
