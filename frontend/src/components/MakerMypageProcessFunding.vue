f0fcf8e4e561486e6a6123760f3c888c78e822f1
Kim <rdt2359@gmail.com>
2022-10-06 14:25:55 +0900

    [#S07P22A306-156] fix: Minor Bug Fix

--- frontend/src/components/MakerMypageProcessFunding.vue
+++ frontend/src/components/MakerMypageProcessFunding.vue
@@ -39,14 +39,6 @@
           v-for="funding in fundings"
           :key="funding.id"
           style="margin-right: 30px">
-          <router-link
-            :to="{ path: '/funding/cashout', query: { id: funding.id } }">
-            <img
-              class="coin"
-              src="@/assets/금액.png"
-              style="width: 30px; height: 30px; box-sizing: border-box"
-              alt="" />
-          </router-link>
 
           <div class="thumbnail">
             <router-link
