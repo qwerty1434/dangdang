73bbf9f09c17565302dea61d3cc7d08f7b35ad9d
김윤석 <rdt2359@gmail.com>
2022-10-06 12:19:47 +0900

    [#S07P22A306-110] feat : 유저 이메일 조회 기능 추가

---
+++ backend/src/main/java/com/dangdang/member/dto/UUIDRequest.java
@@ -0,0 +1,12 @@
+package com.dangdang.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UUIDRequest {
+    private String uuid;
+}
