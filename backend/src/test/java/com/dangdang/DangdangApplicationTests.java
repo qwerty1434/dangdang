501158de77012a90587a2af326482d20841f46df
leemyo <leemyo_@naver.com>
2022-10-07 11:39:59 +0900

    self revert

---
+++ backend/src/test/java/com/dangdang/DangdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
